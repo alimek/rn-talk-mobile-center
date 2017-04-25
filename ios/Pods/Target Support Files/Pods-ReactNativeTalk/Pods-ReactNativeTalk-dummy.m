91ebc0989d07a3e1c1e347ba208eb42545b62f6f
Grzegorz Mandziak <alimek@MacBook-Pro-Grzegorz.local>
2017-04-26 00:09:56 +0200

    required deps

---
+++ ios/Pods/Target Support Files/Pods-ReactNativeTalk/Pods-ReactNativeTalk-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReactNativeTalk : NSObject
+@end
+@implementation PodsDummy_Pods_ReactNativeTalk
+@end
---
+++ ios/Pods/Target Support Files/Pods-ReactNativeTalkTests/Pods-ReactNativeTalkTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReactNativeTalkTests : NSObject
+@end
+@implementation PodsDummy_Pods_ReactNativeTalkTests
+@end
